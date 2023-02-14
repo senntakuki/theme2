9a14b9cc17246dd185efe5ca0a1e910a8da8e9dd
hiroki <you@example.com>
2023-02-14 12:12:43 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def new
+  @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    flash[:notice] = "Book was successfully created."
+   if @book.save
+    redirect_to book_path(@book.id)
+   else
+     @books = Book.all
+     render :index
+   end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+     flash[:notice] = "Book was successfully updated."
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    flash[:notice] = "Book was successfully destroyed"
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
